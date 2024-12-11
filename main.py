dd729e0b0b8dd7db2cf25b828a4aef51028d6590
kaiser-lai <klai2027@kehillahstudent.org>
2024-12-11 11:43:27 -0800

    done

--- main.py
+++ main.py
@@ -2,6 +2,53 @@
 from simulator import robot
 import time
 
-left, right = robot.sonars()
-robot.motors(1, 1, 2)
- 
+
+def f (distance):
+    robot.motors(1 ,1, distance)
+    return 1
+
+def b (distance):
+    robot.motors(-1, -1, distance)
+
+def l (distance):
+    robot.motors(1, -1, distance)
+
+def r (distance):
+    robot.motors(-1, 1, distance)
+
+def s ():
+    right, left = robot.sonars()
+    print(left, right)
+    return left, right
+
+
+while True:
+    exit = input(" do you wish to continue y/n?")
+    if exit == "y":
+
+        user = int(input("how long u want to move?"))
+
+        f(user)
+        r(4)
+        l(4)
+        b(user)
+        left, right = s()
+    elif exit == "n":
+        robot.exit()
+    else:
+        print("speak human")
+    
+
+
+    
+
+
+
+
+    
+
+
+
+    
+
+
